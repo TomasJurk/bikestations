e88385041e48609c9e885d4d67b9576902738799
Tom <tomasstj@gmail.com>
2021-01-23 14:51:06 +0100

    stations fetching with axios and fintering done

--- src/components/Station/Station.js
+++ src/components/Station/Station.js
@@ -9,10 +9,11 @@ const Station = (props) => {
 
     return (
         <div className={[styles.Station, props.selected ? styles.Station_Selected : null].join(' ')} onClick={props.select}>
-            <h4>Station: "{props.name}"</h4>
+            <h4 className={props.selected ? styles.Station_Name : null}>Station: {props.name}</h4>
             <div className={['spacer', styles.Station_Info, props.selected ? null : styles.Station_Info_Fade].join(' ')}>
                 <p>Address: {props.address}</p>
-
+                <p>Available bikes: {props.availBike}</p>
+                <p>Available docks: {props.availDock}</p>
                 <a className={styles.MapLink} href={googleMapLink} target="_blank" rel="noreferrer">Show on map</a>
             </div>
         </div>
--- src/containers/Homepage/Homepage.js
+++ src/containers/Homepage/Homepage.js
@@ -1,99 +1,50 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import styles from './Homepage.module.css';
 import Station from '../../components/Station/Station';
-import StationsMap from '../../components/StationsMap/StationsMap';
 import Navbar from '../../components/Navigation/Navbar/Navbar';
 
 class Homepage extends Component {
 
     state = {
-        stations: [
-            {
-              "station_id": "2280",
-              "name": "Klingenberggata",
-              "address": "Olav Vs Gate 5",
-              "lat": 59.913150534075015,
-              "lon": 10.732281291700133,
-              "availability": {
-                "num_bikes_available": 2
-              },
-              selected: false
-            },
-            {
-              "station_id": "2270",
-              "name": "Lørenporten",
-              "address": "Lørenveien 63",
-              "lat": 59.92883258499495,
-              "lon": 10.799770383800876,
-              "availability": {
-                "num_bikes_available": 1
-              },
-              selected: false
-            },
-            {
-              "station_id": "1919",
-              "name": "Kværnerveien",
-              "address": "Kværnerveien 5",
-              "lat": 59.90591083488326,
-              "lon": 10.778592132296495,
-              "availability": {
-                "num_bikes_available": 0
-              },
-              selected: false
-            },
-            {
-              "station_id": "1755",
-              "name": "Aker Brygge",
-              "address": "Aker Brygge",
-              "lat": 59.91118372188379,
-              "lon": 10.730034556850455,
-              "availability": {
-                "num_bikes_available": 3
-              },
-              selected: false
-            },
-            {
-              "station_id": "1101",
-              "name": "Stortingstunellen",
-              "address": "Rådhusgata 34",
-              "lat": 59.91065301806209,
-              "lon": 10.737365277561025,
-              "availability": {
-                "num_bikes_available": 5
-              },
-              selected: false
-            },
-            {
-              "station_id": "1023",
-              "name": "Professor Aschehougs plass",
-              "address": "Professor Aschehougs plass",
-              "lat": 59.9147672,
-              "lon": 10.740971,
-              "availability": {
-                "num_bikes_available": 2
-              },
-              selected: false
-            },
-            {
-              "station_id": "1009",
-              "name": "Borgenveien",
-              "address": "Slemsdalsveien 70B",
-              "lat": 59.942742106473666,
-              "lon": 10.703833031254021,
-              "availability": {
-                "num_bikes_available": 1
-              },
-              selected: false
-            }
-        ],
+        stations: [],
         loading: true,
-        error: null
+        error: false
     }
 
     componentDidMount() {
-        // fetch stations
-        // .filter(station => station['availability']['num_bikes_available'] ? true : false) USE HERE
+        // res.data.data.stations.filter(station => station['availability']['num_bikes_available'] ? true : false)
+
+        const body = {
+            query: `{
+                stations {
+                    station_id
+                    name
+                    address
+                    lat
+                    lon
+                    availability {
+                        num_bikes_available
+                        num_docks_available
+                    }
+                }
+            }`
+        } 
+
+        axios.post('https://gbfs-graphql.vercel.app/api/graphql', body)
+            .then(res => {
+                const stations = res.data.data.stations.filter(station => {
+                    if (station['availability']['num_bikes_available'] && station['availability']['num_docks_available']) {
+                        station.selected = false;
+                        return true;
+                    }
+                    return false;
+                });
+                console.log(stations)
+                this.setState({stations: stations})
+            })
+            .catch(err => this.setState({error: true}))
     }
 
     selectStationHandler = (i) => {
@@ -108,12 +59,11 @@ class Homepage extends Component {
 
     render() {
         let stations = null;
-
+        console.time('Try')
         if (this.state.error) {
             stations = (
                 <div>
-                    <p style={{textAlign: 'center'}}>Something went wrong!</p>
-                    <p style={{textAlign: 'center'}}>{this.state.error}</p>
+                    <h3 style={{textAlign: 'center', color: 'red'}}>Something went wrong!</h3>
                 </div>
             );
         }
@@ -127,18 +77,18 @@ class Homepage extends Component {
                                 address={station['address']}
                                 lat={station['lat']}
                                 lon={station['lon']}
-                                availability={station['availability']['num_bikes_available']}
+                                availBike={station['availability']['num_bikes_available']}
+                                availDock={station['availability']['num_docks_available']}
                                 selected={station.selected}
                                 select={() => this.selectStationHandler(i)} />
                 });
         }
-
+        console.timeEnd('Try')
         return (
             <div className={styles.Homepage}>
                 <Navbar />
                 <h1 className={styles.Header}>Available bikes at:</h1>
                 <main>
-                    <StationsMap />
                     <div className={styles.StationsList}>
                         {stations}
                     </div>
